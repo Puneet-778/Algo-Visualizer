261d23eee039637bb7f09cc8d3c1668b878fccd6
Puneet-778 <63799563+Puneet-778@users.noreply.github.com>
2021-06-27 20:18:37 +0530

    Update linking.js

--- js/linking.js
+++ js/linking.js
@@ -13,6 +13,14 @@ document.querySelector("#home").addEventListener("click", function () {
   window.location.assign(`./index.html`);
 });
 
+// if dijikstra is selected create weighted graph-----------------------------
+// ----------------------------------------------------------------------------
+// common code-----------------------------------------------------------------
+// cant get the weighted graph locatio.reload()--> on reset button 
+if(selectedAlgo == 'dijikstra'){
+  createWeightedGraph(graph);
+}
+
 
 
 
